dc3754f37a6be803ee0445044d5594a389337f4e
Siddhant <siddhant.saxena@nexityconsulting.com>
2026-07-27 18:45:24 +0530

    SSEP-368 added pagination in it

--- broker_app/mobile_api/broker.py
+++ broker_app/mobile_api/broker.py
@@ -413,11 +413,13 @@ def get_purchase_orders(filters=None, fields=None, start=0, page_length=20):
 import frappe
 
 @frappe.whitelist()
-def get_party_suppliers():
+def get_party_suppliers(start=0, limit=20):
     suppliers = frappe.get_all(
         "Supplier",
         filters={"custom_is_party": 1},
-        fields=["name", "supplier_name"]
+        fields=["name", "supplier_name"],
+        limit_start=start,
+        limit_page_length=limit
     )
 
     return [
